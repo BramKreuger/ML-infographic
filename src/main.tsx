751b49a5329b07edf420e9de3d041ba4011b7c34
Bram Kreuger <bramkreuger@gmail.com>
2025-10-19 11:38:06 +0200

    Migrate from single file to project

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AircraftTimeline from './aircraft-timeline'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AircraftTimeline />
+  </React.StrictMode>,
+)
